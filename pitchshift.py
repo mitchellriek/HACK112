363691fb9f8abff256ebe593dccc79ee3537bd55
thecozysweaters <aquaboi5@gmail.com>
2016-04-02 00:35:12 -0400

    Added files via upload

--- pitchshift.py
+++ pitchshift.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pydub import *
 
 def pitchShift(array, shiftFactor):
     # takes an array and shifts the pitch up or down one octave
@@ -11,6 +12,14 @@ def pitchShift(array, shiftFactor):
             newArray = newArray + 2*[array[i]]
     return newArray
             
-            
+
 print(pitchShift([2,1,0,1,2,1,0,1,2,1,0,1], -1))
 print(pitchShift([2,1,0,1,2,1,0,1,2,1,0,1], 1))
+
+def recursiveEcho(track, deltaVolume = 0, delayLength=200):
+    if volume <= -10:
+        return track
+    else:
+        silence = AudioSegment.silent(duration=delayLength)
+        currentTrack = silence + track
+        return (currentTrack.overlay(recursiveEcho(track, deltaVolume - 2, delayLength)))
